a40ff5a2064565413e20324e7435d18bdddf842f
Alphonsa <alphonsa@techvantagesystems.com>
2018-09-14 15:51:07 +0530

    Added location,silver and bronze values and link to get information about stack overflow users with ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_stack_overflow_api/repository.dart';
 import 'package:flutter_stack_overflow_api/user.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     Repository repository = Repository();
 
     return new MaterialApp(
       title: 'Stackoverflow',
       theme: new ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      home: new MyHomePage(title: 'Stackoverflow Users', repository: repository),
+      home:
+          new MyHomePage(title: 'Stackoverflow Users', repository: repository),
     );
   }
 }
@@ -36,7 +37,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Stackoverflow Users"),
+        centerTitle: true,
+        title: Text(
+          "Stackoverflow Buddies",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: FutureBuilder<List<User>>(
         future: widget.repository.getStackOverflowUsers(),
@@ -57,9 +62,72 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget _buildUserWidget(User user) {
     return Container(
-      child: ListTile(
-        title: Text(user.name),
-        subtitle: Text("${user.reputation}"),
+      child: ExpansionTile(
+        leading: CircleAvatar(
+          radius: 30.0,
+          backgroundImage: NetworkImage(user.profilePicUrl),
+        ),
+        title: ListTile(
+            title: Text(user.name),
+            subtitle: Row(children: <Widget>[
+              Icon(Icons.location_on),
+              Expanded(
+                child: Text(
+                  user.location ?? "",
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ])),
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('assets/gold.png'),
+                        ),
+                        Text("${user.gold}")
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('assets/silver.png'),
+                        ),
+                        Text("${user.silver}")
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('assets/bronze.png'),
+                        ),
+                        Text("${user.bronze}")
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: new InkWell(
+                  child: new Text(
+                    "For more details about ${user.name}, Tap here",
+                    style: new TextStyle(color: Colors.blue),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  onTap: () => launch(user.link),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
